b464d7f2a9578edebb09cdaa0083df2c8890ec37
edanielj <edanielj@gmail.com>
2021-04-06 18:28:19 +0530

    Added Variables

--- main.tf
+++ main.tf
@@ -1,12 +1,8 @@
-provider "aws" {
-  region = "us-east-1"
-}
-
 resource "aws_instance" "myec2" {
-  ami = "ami-0742b4e673072066f"
-  instance_type = "t2.micro"
+  ami           = var.amiid
+  instance_type = var.instance_type
   tags = {
-    "Name" = "Day1_EC2"
+    "Name" = var.tags
   }
 }
 
---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "amiid" {
+  description = "The AMI ID"
+  type        = string
+  default     = "ami-0742b4e673072066f"
+}
+variable "instance_type" {
+  description = "Type of instance"
+  type        = string
+  default     = "t2.micro"
+}
+variable "tags" {
+  description = "Tags"
+  default     = "RT_APP"
+}
